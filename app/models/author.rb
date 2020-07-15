a1efd511148ee5c34c383d153acb211bdcd82057
Joshua McArthur <jcmcarth06@gmail.com>
2020-07-15 20:28:19 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ActiveRecord::Base
-  validate :name, presence: true { error: please enter a name}
-  validate :email, uniqueness: true { error: email already in use}
+  validates :name, presence: true { error: 'please enter a name' }
+  validates :email, uniqueness: true { error: 'email already in use' }
 end
