884c663475de58a3b46cc604c396712e541ea0a5
Joshua McArthur <jcmcarth06@gmail.com>
2020-07-15 20:39:54 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,7 +7,7 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = author.new(author_params)
+    @author = Author.new(author_params)
     if @author.valid?
       @author.save
       redirect_to author_path(@author)
